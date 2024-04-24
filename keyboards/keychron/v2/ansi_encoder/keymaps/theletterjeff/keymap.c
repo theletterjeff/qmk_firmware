9200e2808f10e8f55f0cdf9523b6dcbab5e53675
Jeff Martin <jmartin@vistarmedia.com>
2024-04-24 10:53:20 -0400

    deactivate old ent/bspc in mac base and add enter to fn layer

--- keyboards/keychron/v2/ansi_encoder/keymaps/theletterjeff/keymap.c
+++ keyboards/keychron/v2/ansi_encoder/keymaps/theletterjeff/keymap.c
@@ -27,8 +27,8 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_67(
         KC_ESC,                KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_MUTE,
-        LT(FN, KC_TAB),        KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,  XXXXXXX,
-        KC_LEFT_CTRL,             KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  KC_ENT,   XXXXXXX,
+        LT(FN, KC_TAB),        KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,  XXXXXXX,  XXXXXXX,
+        KC_LEFT_CTRL,             KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  XXXXXXX,   XXXXXXX,
         KC_LEFT_SHIFT,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,  KC_UP,
         TG(FN),             KC_LOPT,  KC_LCMD,  KC_SPC,  KC_CAPS_LOCK,  TG(MAC_BASE),  TG(MEDIA), KC_LEFT,  KC_DOWN,  KC_RIGHT),
 
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [FN] = LAYOUT_ansi_67(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,       KC_F4,         KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,           KC_F11,   KC_F12,   XXXXXXX,  _______,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,       XXXXXXX,  XXXXXXX,  KC_BSPC,  XXXXXXX,  XXXXXXX,   KC_PRINT_SCREEN,  XXXXXXX,  XXXXXXX,  KC_DEL,   XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,       XXXXXXX,  XXXXXXX,  KC_BSPC,  KC_ENT,  XXXXXXX,   KC_PRINT_SCREEN,  XXXXXXX,  XXXXXXX,  KC_DEL,   XXXXXXX,
         XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_HOME,     KC_END,        XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  XXXXXXX,          XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PAGE_UP,  KC_PAGE_DOWN,  XXXXXXX,  KC_WWW_BACK,  KC_WWW_FORWARD,  XXXXXXX,  XXXXXXX,   XXXXXXX,          XXXXXXX,  XXXXXXX,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX),
